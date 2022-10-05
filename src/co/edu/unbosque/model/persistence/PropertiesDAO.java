bb46e571f727c1199ee827ce8f36a4b0136e7e34
DevJohann <felipepoveda273@gmail.com>
2022-10-05 17:21:39 -0500

    reparacion

--- src/co/edu/unbosque/model/persistence/PropertiesDAO.java
+++ src/co/edu/unbosque/model/persistence/PropertiesDAO.java
@@ -28,7 +28,9 @@ public class PropertiesDAO {
 			System.out.println("Archivo no encontrado");
 		} catch(IOException e) {
 			System.out.println("Error cargando el archivo");
-
+		} catch(NullPointerException e) {
+			System.out.println("La ruta no fue cargada");
+		}
 		
 	}
 	
